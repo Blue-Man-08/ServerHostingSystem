9cb46e24a5ccf6119e65dbf6888e416f5a79a009
Blue-Man-08 <vedantprasadreddy@gmail.com>
2025-05-13 13:39:09 -0700

    v1.21 - Updated Bat files

---
+++ Bat Files/onstartup.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Prasad\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\Prasad\Desktop\Discord Bots\FHS Monkeys\FHS Monkeys Bot.py"
+pause
---
+++ Bat Files/serverstop.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Prasad\Desktop\Discord Bots\FHS Monkeys
+taskkill /IM "playit.exe" /f
+call "C:\Users\Prasad\Desktop\Discord Bots\FHS Monkeys\sendkeys.bat" "Minecraft server" "{TAB} {TAB} stop {ENTER}"
